96b227cdf448934c632603d81d976a8a7d4717f0
Pontus <contrib@pontusnetworks.com>
2018-05-01 14:40:05 +0100

    added missing import for jquery

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Keycloak from "keycloak-js";
 import axios from "axios";
+import $ from "jquery";
+
 
 // import './index.css';
 
