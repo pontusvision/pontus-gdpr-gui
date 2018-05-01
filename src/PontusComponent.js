61d8616451c5966b533157fb606075beaf946b5d
Pontus <contrib@pontusnetworks.com>
2018-05-01 18:51:35 +0100

    changed URL logic

--- src/PontusComponent.js
+++ src/PontusComponent.js
@@ -62,6 +62,15 @@ class PontusComponent extends React.Component
     {
       return props.url;
     }
+    else if (window.location && window.location.pathname){
+      let pvgdprGuiIndex = window.location.pathname.indexOf(pvgdprGuiStr)
+      if (pvgdprGuiIndex > 0)
+      {
+        let retVal = window.location.pathname.substr(0, pvgdprGuiIndex);
+        retVal.concat(defaultSuffix);
+        return retVal;
+      }
+    }
     else if (props.baseURI)
     {
       if (props.ownerDocument && props.ownerDocument.origin)
@@ -81,10 +90,11 @@ class PontusComponent extends React.Component
         }
       }
     }
+    
     return defaultSandbox;
   }
   
 }
 
-
+ 
 export default PontusComponent;
