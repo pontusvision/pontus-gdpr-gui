c4db1ce9a6933eeb886476681754c30cdb2c300b
Leonardo Martins <lmartins@pontusnetworks.com>
2018-11-04 18:39:20 +0000

    fixed bug in the NavPanel Dataprotection officer popup score logic

--- src/NavPanelDataProtnOfficerPopup.js
+++ src/NavPanelDataProtnOfficerPopup.js
@@ -41,137 +41,135 @@ class NavPanelDataProtnOfficerPopup extends PVGDPRScores
     
     return {
       gremlin: "\n" +
-      "long numDPOs = g.V().has('Person.Employee.Role',eq('Data Protection Officer'))\n" +
-      ".count().next()\n" +
-      " \n" +
-      " \n" +
-      "long numDPODirectReports = g.V().has('Person.Employee.Role',eq('Data Protection Officer')).inE('Reports_To')\n" +
-      ".count().next()\n" +
       "\n" +
-      "\n" +
-      "long numDPOsFailed  = g.V().has('Person.Employee.Role',eq('Data Protection Officer'))\n" +
-      ".in().has('Event.Training.Status',eq('Failed'))\n" +
-      ".count().next()\n" +
-      " \n" +
-      "\n" +
-      "long numDPODirectReportsFailed = g.V().has('Person.Employee.Role',eq('Data Protection Officer')).inE('Reports_To')\n" +
-      ".outV().in().has('Event.Training.Status',eq('Failed'))\n" +
-      ".count().next()\n" +
-      "\n" +
-      "\n" +
-      "long numDPOsSecondReminder  = g.V().has('Person.Employee.Role',eq('Data Protection Officer'))\n" +
-      ".in().has('Event.Training.Status',eq('Second  Reminder'))\n" +
-      ".count().next()\n" +
-      " \n" +
-      "\n" +
-      "long numDPODirectReportsSecondReminder = g.V().has('Person.Employee.Role',eq('Data Protection Officer')).inE('Reports_To')\n" +
-      ".outV().in().has('Event.Training.Status',eq('Second  Reminder'))\n" +
-      ".count().next()\n" +
-      "\n" +
-      "\n" +
-      "\n" +
-      "\n" +
-      " long scoreValue = 100L;\n" +
-      "if (numDPOs > 0){\n" +
-      "  \n" +
-      "  scoreValue -= (long) (25L + 25L*numDPOsFailed/numDPOs);\n" +
-      "  scoreValue -= (long) (13L + 12L*numDPODirectReportsFailed/numDPODirectReports);\n" +
-      "  \n" +
-      "  scoreValue -= (long) (6L + 7L*numDPOsSecondReminder/numDPOs);\n" +
-      "  scoreValue -= (long) (6L + 6L*numDPODirectReportsSecondReminder/numDPODirectReports);\n" +
-      "\n" +
-      "\n" +
-      "\n" +
-      "\n" +
-      " \n" +
-      "}else{\n" +
-      "  scoreValue = 0L; \n" +
-      "}\n" +
-      "\n" +
-      "StringBuffer sb = new StringBuffer ('{ \"scoreValue\": ');\n" +
-      "\n" +
-      "sb.append(scoreValue)\n" +
-      "  .append(', \"scoreExplanation\":\"');\n" +
-      "if (numDPOs > 0)  {\n" +
-      "  sb.append('This score reflects that out of ');\n" +
-      "  sb.append(numDPOs);\n" +
-      "  sb.append(' Data Protection Officer(s), ');\n" +
-      "  if (numDPOsFailed == 0){\n" +
-      "    sb.append( 'NONE have failed the GDPR Awareness Test, and ');\n" +
-      "  }\n" +
-      "  else {\n" +
-      "    sb.append(numDPOsFailed);\n" +
-      "    if (numDPOsFailed == 1){\n" +
-      "      sb.append( ' has failed the GDPR Awarenss Test, and ');\n" +
-      "    }\n" +
-      "    else{\n" +
-      "      sb.append(' have failed the GDPR Awarenss Test, and ');\n" +
-      "    }\n" +
-      "  }\n" +
-      "    \n" +
-      "    \n" +
-      "  if (numDPOsSecondReminder == 0){\n" +
-      "    sb.append( 'NONE have received a second reminder to take the test.');\n" +
-      "  }\n" +
-      "  else {\n" +
-      "    sb.append(numDPOsSecondReminder);\n" +
-      "    if (numDPOsSecondReminder == 1){\n" +
-      "      sb.append( ' has received a second reminder to take the test.');\n" +
-      "    }\n" +
-      "    else{\n" +
-      "      sb.append(' have received a second reminder to take the test.');\n" +
-      "    }\n" +
-      "  }\n" +
-      "\n" +
-      "\n" +
-      "}\n" +
-      "else {\n" +
-      "  sb.append ('There are no Data Protection Officers listed in the system.');\n" +
-      "}\n" +
-      "\n" +
-      "if (numDPODirectReports > 0){\n" +
-      "  sb.append (\"  Out of the \").append(numDPODirectReports);\n" +
-      "  if (numDPODirectReports == 1){\n" +
-      "    sb.append(\" Data Protection Officers Direct Report, \");\n" +
-      "  }\n" +
-      "  else{\n" +
-      "    sb.append(\" Data Protection Officers Direct Reports, \");\n" +
-      "  }\n" +
-      "  \n" +
-      "  if (numDPODirectReportsFailed == 0){\n" +
-      "    sb.append (\"NONE have failed the test, and \");\n" +
-      "  }\n" +
-      "  else{\n" +
-      "    sb.append(numDPODirectReportsFailed);\n" +
-      "    if (numDPODirectReportsFailed == 1){\n" +
-      "      sb.append (\" has failed the test, and \");\n" +
-      "    }\n" +
-      "    else{\n" +
-      "      sb.append (\" have failed the test, and \");\n" +
-      "    }\n" +
-      "  }\n" +
-      "  \n" +
-      "  if (numDPODirectReportsSecondReminder == 0){\n" +
-      "    sb.append (\"NONE have received a second test reminder.\");\n" +
-      "  }\n" +
-      "  else{\n" +
-      "    sb.append(numDPODirectReportsSecondReminder);\n" +
-      "    if (numDPODirectReportsSecondReminder == 1){\n" +
-      "      sb.append (\" has received a second test reminder.\");\n" +
-      "    }\n" +
-      "    else{\n" +
-      "      sb.append (\" have received a second test reminder.\");\n" +
-      "    }\n" +
-      "  }\n" +
-      "  \n" +
-      "  \n" +
-      "}\n" +
-      "\n" +
-      "\n" +
-      "\n" +
-      "sb.append('\" }')  \n" +
-      "\n" +
-      "sb.toString()\n"
+        "long numDPOs = g.V().has('Person.Employee.Role',eq('Data Protection Officer'))\n" +
+        ".count().next()\n" +
+        " \n" +
+        " \n" +
+        "long numDPODirectReports = g.V().has('Person.Employee.Role',eq('Data Protection Officer')).inE('Reports_To')\n" +
+        ".count().next()\n" +
+        "\n" +
+        "\n" +
+        "long numDPOsFailed  = g.V().has('Person.Employee.Role',eq('Data Protection Officer'))\n" +
+        ".in().has('Event.Training.Status',eq('Failed'))\n" +
+        ".count().next()\n" +
+        " \n" +
+        "\n" +
+        "long numDPODirectReportsFailed = g.V().has('Person.Employee.Role',eq('Data Protection Officer')).inE('Reports_To')\n" +
+        ".outV().in().has('Event.Training.Status',eq('Failed'))\n" +
+        ".count().next()\n" +
+        "\n" +
+        "\n" +
+        "long numDPOsSecondReminder  = g.V().has('Person.Employee.Role',eq('Data Protection Officer'))\n" +
+        ".in().has('Event.Training.Status',eq('Second  Reminder'))\n" +
+        ".count().next()\n" +
+        " \n" +
+        "\n" +
+        "long numDPODirectReportsSecondReminder = g.V().has('Person.Employee.Role',eq('Data Protection Officer')).inE('Reports_To')\n" +
+        ".outV().in().has('Event.Training.Status',eq('Second  Reminder'))\n" +
+        ".count().next()\n" +
+        "\n" +
+        "\n" +
+        "\n" +
+        "\n" +
+        "long scoreValue = 100L;\n" +
+        "if (numDPOs > 0){\n" +
+        "  scoreValue -= (long) (25L + 25L*numDPOsFailed/numDPOs);\n" +
+        "  scoreValue -= (long) (6L + 7L*numDPOsSecondReminder/numDPOs);\n" +
+        "}\n" +
+        "if (numDPODirectReports > 0){\n" +
+        "  scoreValue -= (long) (13L + 12L*numDPODirectReportsFailed/numDPODirectReports);\n" +
+        "  \n" +
+        "  scoreValue -= (long) (6L + 6L*numDPODirectReportsSecondReminder/numDPODirectReports);\n" +
+        "}\n" +
+        "if (numDPOs == 0 && numDPODirectReports == 0){\n" +
+        "  scoreValue = 0L; \n" +
+        "}\n" +
+        "\n" +
+        "StringBuffer sb = new StringBuffer ('{ \\\"scoreValue\\\": ');\n" +
+        "\n" +
+        "sb.append(scoreValue)\n" +
+        "  .append(', \\\"scoreExplanation\\\":\\\"');\n" +
+        "if (numDPOs > 0)  {\n" +
+        "  sb.append('This score reflects that out of ');\n" +
+        "  sb.append(numDPOs);\n" +
+        "  sb.append(' Data Protection Officer(s), ');\n" +
+        "  if (numDPOsFailed == 0){\n" +
+        "    sb.append( 'NONE have failed the GDPR Awareness Test, and ');\n" +
+        "  }\n" +
+        "  else {\n" +
+        "    sb.append(numDPOsFailed);\n" +
+        "    if (numDPOsFailed == 1){\n" +
+        "      sb.append( ' has failed the GDPR Awarenss Test, and ');\n" +
+        "    }\n" +
+        "    else{\n" +
+        "      sb.append(' have failed the GDPR Awarenss Test, and ');\n" +
+        "    }\n" +
+        "  }\n" +
+        "    \n" +
+        "    \n" +
+        "  if (numDPOsSecondReminder == 0){\n" +
+        "    sb.append( 'NONE have received a second reminder to take the test.');\n" +
+        "  }\n" +
+        "  else {\n" +
+        "    sb.append(numDPOsSecondReminder);\n" +
+        "    if (numDPOsSecondReminder == 1){\n" +
+        "      sb.append( ' has received a second reminder to take the test.');\n" +
+        "    }\n" +
+        "    else{\n" +
+        "      sb.append(' have received a second reminder to take the test.');\n" +
+        "    }\n" +
+        "  }\n" +
+        "\n" +
+        "\n" +
+        "}\n" +
+        "else {\n" +
+        "  sb.append ('There are no Data Protection Officers listed in the system.');\n" +
+        "}\n" +
+        "\n" +
+        "if (numDPODirectReports > 0){\n" +
+        "  sb.append (\\\"  Out of the \\\").append(numDPODirectReports);\n" +
+        "  if (numDPODirectReports == 1){\n" +
+        "    sb.append(\\\" Data Protection Officers Direct Report, \\\");\n" +
+        "  }\n" +
+        "  else{\n" +
+        "    sb.append(\\\" Data Protection Officers Direct Reports, \\\");\n" +
+        "  }\n" +
+        "  \n" +
+        "  if (numDPODirectReportsFailed == 0){\n" +
+        "    sb.append (\\\"NONE have failed the test, and \\\");\n" +
+        "  }\n" +
+        "  else{\n" +
+        "    sb.append(numDPODirectReportsFailed);\n" +
+        "    if (numDPODirectReportsFailed == 1){\n" +
+        "      sb.append (\\\" has failed the test, and \\\");\n" +
+        "    }\n" +
+        "    else{\n" +
+        "      sb.append (\\\" have failed the test, and \\\");\n" +
+        "    }\n" +
+        "  }\n" +
+        "  \n" +
+        "  if (numDPODirectReportsSecondReminder == 0){\n" +
+        "    sb.append (\\\"NONE have received a second test reminder.\\\");\n" +
+        "  }\n" +
+        "  else{\n" +
+        "    sb.append(numDPODirectReportsSecondReminder);\n" +
+        "    if (numDPODirectReportsSecondReminder == 1){\n" +
+        "      sb.append (\\\" has received a second test reminder.\\\");\n" +
+        "    }\n" +
+        "    else{\n" +
+        "      sb.append (\\\" have received a second test reminder.\\\");\n" +
+        "    }\n" +
+        "  }\n" +
+        "  \n" +
+        "  \n" +
+        "}\n" +
+        "\n" +
+        "\n" +
+        "\n" +
+        "sb.append('\\\" }')  \n" +
+        "\n" +
+        "sb.toString()\n" 
       
       , bindings: {
         // pg_from: from
